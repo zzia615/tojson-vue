51518bbf148c1c92c8b1fd5ce6e956356c7c207c
jiaolong <zzia615@163.com>
2023-08-02 10:01:01 +0800

    新增

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import JsonViewer from 'vue-json-viewer'
 Vue.config.productionTip = false
-
+Vue.use(JsonViewer)
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <textarea rows="5" v-model="json"></textarea>
+    <div class="viewerContainer">
+      <json-viewer :value="json?JSON.parse(json):null"></json-viewer>
+    </div>
+  </div>
+</template>
+<style scoped>
+  textarea{
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    bottom: 5px;
+    width: 400px;
+  }
+  .viewerContainer{
+    position: absolute;
+    top: 5px;
+    left: 420px;
+    bottom: 5px;
+    right: 5px;
+    border: solid #000 0.8px;
+    overflow: auto;
+  }
+</style>
+<script>
+// @ is an alias to /src
+
+export default {
+  data(){
+    return{
+      json:''
+    }
+  }
+}
+</script>
